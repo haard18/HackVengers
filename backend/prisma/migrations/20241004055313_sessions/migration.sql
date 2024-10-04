3dd641384198c648ee0f11cb7ee3723b32f55eae
haard18 <haardsolanki.itm@gmail.com>
2024-10-04 11:24:07 +0530

    Remove 'token' column from 'Trainer' table

---
+++ backend/prisma/migrations/20241004055313_sessions/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `token` on the `Trainer` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Trainer" DROP COLUMN "token";
